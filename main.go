6334d14b5e436b24d1f3c1aefd99015035f29a40
Berik Yeraliyev <eraliev237@gmail.com>
2025-02-08 14:24:11 +0500

    feat: add CLI command handling

--- main.go
+++ main.go
@@ -2,20 +2,60 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	// To run the program you need to write the following in the command line: “go run main.go <args...>”
+	switch os.Args[1] {
+	case "add":
+		{
+			fmt.Println("add")
+		}
+	case "update":
+		{
+			fmt.Println("update")
+		}
+	case "delete":
+		{
+			fmt.Println("delete")
+		}
+	case "mark-in-progress":
+		{
+			fmt.Println("mark-in-progress")
+		}
+	case "mark-done":
+		{
+			fmt.Println("mark-done")
+		}
+	case "list":
+		{
+			if len(os.Args) > 2 {
+				switch os.Args[2] {
+				case "done":
+					{
+						fmt.Println("done")
+					}
+				case "to-do":
+					{
+						fmt.Println("to-do")
+					}
+				case "in-progress":
+					{
+						fmt.Println("in-progress")
+					}
+				default:
+					{
+						fmt.Println("unknown command")
+					}
+				}
+			} else {
+				fmt.Println("list")
+			}
+		}
+	default:
+		{
+			fmt.Println("unknown command")
+		}
 	}
 }
